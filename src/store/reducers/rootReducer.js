e8fca2b3a67618f55c79c215686f1a5278b514c4
pogybkoivan <griny@ua.fm>
2018-06-05 20:29:45 +0300

    ok!

---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import feed from "./feed";
+import slide from "./slide";
+
+export default combineReducers({
+    feed,
+    slide
+})
---
+++ src/store/reducers/slide.js
@@ -0,0 +1,7 @@
+const initialState = {
+    cursor: 0
+};
+
+export default function slide(state = initialState) {
+    return state
+}
