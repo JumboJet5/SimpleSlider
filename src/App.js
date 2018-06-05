c6a517c9863604acf0947240ca8b2c853c6b73c1
pogybkoivan <griny@ua.fm>
2018-06-05 19:18:37 +0300

    ok!

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+          <image src="https://placeimg.com/150/150/tech/sepia"/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload. Or not!
         </p>
